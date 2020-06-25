8bbbd415531839948ce5c4546b5ddf0d87a9e306
Akira-2307 <a.sugano.sys19@morijyobi.ac.jp>
2020-06-25 11:19:14 +0900

    edit

---
+++ update.php
@@ -0,0 +1,24 @@
+<?php
+$dsn = 'mysql:dbname=sample_db;host=localhost;';
+$user = 'sugano';
+$password = 'Morijyobi';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+​
+    $sql = "update user set name = :name, age = :age where id = id";
+    $stmt = $sbn->prepare($sql);
+    $params = array(';id' => $id, ':name' => $name, ':age' => $age);
+    $atmt->execute($params);
+
+    header('Location: index.php?fg-1');
+} catch (PDOException $e) {
+    //echo "接続失敗: " . $e->getMessage() . "\n";
+    header('Location: index.php?fg = 1? err=$e -> getMessage()');
+    exit();
+}
+?>
